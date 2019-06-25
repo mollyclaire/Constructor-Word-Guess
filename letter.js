f16e86ef711b2ecafce9c8585d663e7cc6cc495f
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-25 18:17:51 -0500

    The test for letter.js works.

--- letter.js
+++ letter.js
@@ -1,7 +1,10 @@
+// Constructor function that defines the letter the user guesses, a Boolean to test whether
+// it's the correct letter or not, and two other methods.
 function Letter(letter) {
     this.letter = letter;
     this.guessed = false;
 
+    // This method creates a string if a letter is guessed and an underscore if a letter is not guessed.
     this.toString = function() {
         if (this.letter === " ") {
             this.guessed = true;
@@ -11,9 +14,18 @@ function Letter(letter) {
             console.log("_");
         }
     }
+    // This method tests whether the guessed letter is one of the underlying letters in the word.
     this.guess = function(guess) {
         if (guess === this.guess) {
             this.guessed = true;
         }
     }
 }
+
+// Declaring a variable for testing
+var test = new Letter(" ");
+test.toString();
+test.guess();
+
+// This exports the constructor in this file, so that it can be accessed by word.js
+module.exports = Letter;
